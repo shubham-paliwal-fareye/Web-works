e6de64144836dd77f0976508c8639ef526716047
yaghyesh <59774858+yaghyesh@users.noreply.github.com>
2021-06-02 00:39:19 +0530

    Fetched and push into todoItems

--- src/working-demo/index.js
+++ src/working-demo/index.js
@@ -2,6 +2,22 @@ let inputText = document.getElementById("input-text");
 let todoBtn = document.getElementById("todo-btn");
 let list = document.getElementById("list");
 
+
+let todoItems = [];
+
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
+
+  todoItems.push(todo);
+  console.log(todoItems);
+}
+
+
+
 function addListeners() {
   todoBtn.addEventListener("click", submitTODO);
 }
@@ -26,6 +42,33 @@ function renderListWithNewItems(value) {
   li.appendChild(btn);
   list.appendChild(li);
   inputText.innerHTML = "";
+
 }
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .then(function(data) {
+    let tempData = data;
+            return tempData.map(function(entry) {
+                if(todoItems.length < 10 && entry.completed==false){
+                let li = document.createElement("li");
+                let btn = document.createElement("button");
+                btn.textContent = "Delete";
+                btn.addEventListener("click", (ele) => {
+                  console.log(ele, li);
+                  list.removeChild(li);
+                });
+                let span = document.createElement("span");
+                span.innerHTML = `${entry.title}`;
+                addTodo(entry.title);
+                li.className = "list-item";
+                li.appendChild(span);
+                li.appendChild(btn);
+                list.appendChild(li);
+                inputText.innerHTML = "";
+
+            }
+            })
+    });
 
 addListeners();
